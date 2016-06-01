88502a96aab6671e8dbc85b31a799074d24474c3
Alina Nevins <alina.nevins@yale.edu>
2016-06-01 17:03:16 -0400

    Adding lambda and proc

--- week-2/in-class-assignments/function-fizzbuzz.rb
+++ week-2/in-class-assignments/function-fizzbuzz.rb
@@ -20,3 +20,53 @@ a = 1.upto(1000) { |x| fizzbuzz(x) }
 
 # map
 array.map { |x| fizzbuzz(x) }
+
+##############
+
+# lambda
+fblamb = lambda do |x|
+  if x%3 == 0 && x%5 == 0
+    puts "FizzBuzz"
+  elsif x%3 == 0
+    puts "Fizz"
+  elsif x%5 == 0
+    puts "Buzz"
+  else
+    puts x
+  end
+end
+
+# lambda each do
+array = Array(1..1000)
+array.each { |x| fblamb.call(x) }
+
+# lambda upto
+a = 1.upto(1000) { |x| fblamb.call(x) }
+
+# lamda map
+array.map { |x| fblamb.call(x) }
+
+##############
+
+# Proc
+fbproc = Proc.new do |x|
+  if x%3 == 0 && x%5 == 0
+    puts "FizzBuzz"
+  elsif x%3 == 0
+    puts "Fizz"
+  elsif x%5 == 0
+    puts "Buzz"
+  else
+    puts x
+  end
+end
+
+# Proc each do
+array = Array(1..1000)
+array.each { |x| fbproc.call(x) }
+
+# Proc upto
+a = 1.upto(1000) { |x| fbproc.call(x) }
+
+# Proc map
+array.map { |x| fbproc.call(x) }
