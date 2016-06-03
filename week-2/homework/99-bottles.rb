ae96e1cfecef1042e4c1c69238a5658b774c0e30
Alina Nevins <alina.nevins@yale.edu>
2016-06-03 10:34:25 -0400

    99 Bottles HW

--- week-2/homework/99-bottles.rb
+++ week-2/homework/99-bottles.rb
@@ -1,7 +1,6 @@
-def takeonedown(bottles_left)
-puts bottles_left "bottles of beer on the wall, " bottles_left "bottles of beer
-take one down, pass it around, " bottles_left -1 bottles of beer on the wall "
-
-
-  # sing verse
-  # minus by 1
+b = 99 # start with 99 bottles
+while b > 1 # as long as there is at least 1 bottle left
+  puts b.to_s + " bottles of beer on the wall, " + b.to_s + " bottles of beer take one down, pass it around, " + ( b - 1 ).to_s + " bottles of beer on the wall!" # sing the song
+  b -= 1 # one fewer bottle now
+end
+puts "1 bottle of beer on the wall, 1 bottle of beer take one down, pass it around, no more bottles of beer on the wall!" # last bottle
